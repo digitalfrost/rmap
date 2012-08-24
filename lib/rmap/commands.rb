a85075ce5006e9f4c78d23b2f6721db3b3d4d405
Jody Salt <jody_leigh_salt@yahoo.co.uk>
2012-08-24 17:06:29 +0100

    created migrate --to previous|next console feature

--- lib/rmap/commands.rb
+++ lib/rmap/commands.rb
@@ -90,80 +90,107 @@ module Rmap
       
     end
 
-    
     def self.migrate(options = {})
-      db = Rmap::Commands::db
-      
       current_migration = Rmap::Commands::current_migration(:echo => false)
       
       migrations = Dir.new("#{Rmap::CONF_ROOT}/migrations/").to_a.find_all{|file| ::File.file? "#{Rmap::CONF_ROOT}/migrations/#{file}" }.map{ |file| Rmap::Migration.new("#{Rmap::CONF_ROOT}/migrations/#{file}") }.sort {|l,r| l.schema_version <=> r.schema_version}
       
-      if !options[:to].nil?
-        to = options[:to].to_i
-        found = false
-        migrations.each do |migration|
-          if migration.schema_version == to
-            found = true
+      if migrations.count == 0
+        raise "There are currently no migrations."
+      end
+      
+      if options[:to].nil?
+        to = migrations.last.schema_version
+        if to == current_migration
+          raise "There are no are more migrations that can be applied."
+        end
+      elsif options[:to] == 'previous'
+        if current_migration == 0
+          raise "No migrations have been applied."
+        elsif current_migration == migrations.first.schema_version
+          to = 0
+        else
+          migrations.each_with_index do |migration, i|
+            if current_migration == migration.schema_version
+              to = migrations[i - 1]
+              break
+            end
+          end
+        end
+      elsif options[:to] == 'next'
+        if current_migration == 0
+          to = migrations.first.schema_version
+        elsif current_migration == migrations.last.schema_version
+          raise "There are no are more migrations that can be applied."
+        end
+        migrations.each_with_index do |migration, i|
+          if current_migration == migration.schema_version
+            to = migrations[i + 1]
             break
           end
         end
+      else
+        found = false
+        to = options[:to].to_i
         
-        if !found
-          raise "No such migration '#{to}' exists"
+        if to == current_migration
+          raise "Already at migration #{to}"
         end
         
-        if to > current_migration
-          migrations.each do |migration|
-            if migration.schema_version <= current_migration
-              next
-            end
-            
-            if migration.schema_version <= to
-              puts "up: #{migration.schema_version}"
-              db.run &migration.up_block
-            else
+        if to != 0
+          migrations.each do |migration, i|
+            if migration.schema_version == to
+              found = true
               break
             end
           end
-          db.rmap_vars.key_eq(:current_migration).value = to.to_s
-        elsif to < current_migration
-          migrations.reverse.each do |migration|
-            if migration.schema_version > current_migration
-              next
-            end
-            if migration.schema_version > to
-              puts "down: #{migration.schema_version}"
-              db.run &migration.down_block
-            else
-              break
-            end
+          
+          if !found
+            raise "No such migration '#{to}' exists"
           end
-          db.rmap_vars.key_eq(:current_migration).value = to
-        else
-          raise "already at migration #{to}"
         end
-        
-      elsif migrations.count > 0
+      end
+      
+      if to > current_migration
         migrations.each do |migration|
-          if migration.schema_version > current_migration
+          if migration.schema_version <= current_migration
+            next
+          end
+          
+          if migration.schema_version <= to
             puts "up: #{migration.schema_version}"
             db.run &migration.up_block
+          else
+            break
           end
         end
-        db.rmap_vars.key_eq(:current_migration).value = migrations.last.schema_version
+      else
+        migrations.reverse.each do |migration|
+          if migration.schema_version > current_migration
+            next
+          end
+          if migration.schema_version > to
+            puts "down: #{migration.schema_version}"
+            db.run &migration.down_block
+          else
+            break
+          end
+        end
       end
-           
+      
+      db.rmap_vars.key_eq(:current_migration).value = to
+      
     end
   
-  private
-    
-  def self.db
-    db = Rmap::Database.new
-    if Rmap.const_defined? :CONF_ROOT
-      db.run("#{Rmap::CONF_ROOT}/conf.rmap.rb")
+    private
+      
+    def self.db
+      db = Rmap::Database.new
+      if Rmap.const_defined? :CONF_ROOT
+        db.run("#{Rmap::CONF_ROOT}/conf.rmap.rb")
+      end
+      db
     end
-    db
-  end
   
   end
 end
